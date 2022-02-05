68fa19fc160a8a1aedb25c834ddd5b71c6f07b6f
valeria-ben <valeria.benitezs@outlook.com>
2022-02-04 23:58:48 -0600

    home udated, buttons fixed on the menu bar

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("homepage.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
